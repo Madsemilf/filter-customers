f058fea24639c10964e729a23e5604a214e4cb93
Madsemilf <mads@fellkjaer.no>
2023-10-18 10:31:14 +0200

    Imported angular material to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppMaterialModule } from './app-material.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, NoopAnimationsModule],
+  imports: [BrowserModule, AppMaterialModule, NoopAnimationsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
