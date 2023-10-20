eee5c22eaba9e7818a16a9fde617d3c49b59b56f
Madsemilf <mads@fellkjaer.no>
2023-10-20 10:30:54 +0200

    Imported customers-list to parent module

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AppMaterialModule } from '../app-material.module';
 import { CustomersComponent } from './customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 @NgModule({
   imports: [CommonModule, AppMaterialModule],
-  declarations: [CustomersComponent],
+  declarations: [CustomersComponent, CustomersListComponent],
   exports: [CustomersComponent],
 })
 export class CustomersModule {}
