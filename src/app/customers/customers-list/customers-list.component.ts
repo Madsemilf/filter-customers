dc150b115b992a1bc75ca156096aef1611f37a89
Madsemilf <mads@fellkjaer.no>
2023-10-20 15:40:24 +0200

    Imported customers for shared module

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICustomer } from '../shared/interfaces';
+
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
   styleUrls: ['./customers-list.component.scss'],
 })
 export class CustomersListComponent implements OnInit {
-  filteredCustomers: any[] = [];
+  filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number | undefined;
   currencyCode: string = 'USD';
   constructor() {}
