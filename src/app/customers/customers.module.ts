696e1892d7e5a98b5bd00b5c7d2b2f089c787e0c
Madsemilf <mads@fellkjaer.no>
2023-10-20 13:37:49 +0200

    Added filter to DOM and styled they layout

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,10 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from '../app-material.module';
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
+import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
   imports: [CommonModule, AppMaterialModule],
-  declarations: [CustomersComponent, CustomersListComponent],
+  declarations: [
+    CustomersComponent,
+    CustomersListComponent,
+    FilterTextboxComponent,
+  ],
   exports: [CustomersComponent],
 })
 export class CustomersModule {}
