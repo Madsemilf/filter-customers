74baabec4b1f241230865f228b933815f1cdd624
Madsemilf <mads@fellkjaer.no>
2023-10-24 15:42:37 +0200

    Added filter functionality

--- src/app/customers/customers-list/filter-textbox.component.ts
+++ src/app/customers/customers-list/filter-textbox.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'filter-textbox',
-  template: ` Filter: <input type="text" class="filter-input" /> `,
+  template: `
+    Filter: <input type="text" class="filter-input" [(ngModel)]="filter" />
+  `,
   styleUrls: ['./customers-list.component.scss'],
 })
 export class FilterTextboxComponent implements OnInit {
+  private _filter: string = '';
+  @Input() get filter() {
+    return this._filter;
+  }
+
+  set filter(value: string) {
+    this._filter = value;
+    this.changed.emit(this.filter); // Raise changed event
+  }
+
+  @Output() changed: EventEmitter<string> = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit() {}
