0d0e342cdf705674983512a54edca5ee7e735678
Madsemilf <mads@fellkjaer.no>
2023-10-24 15:43:20 +0200

    Created data filtering on customers

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from '../shared/interfaces';
 
@@ -8,49 +8,51 @@ import { ICustomer } from '../shared/interfaces';
   styleUrls: ['./customers-list.component.scss'],
 })
 export class CustomersListComponent implements OnInit {
-  filteredCustomers: ICustomer[] = [];
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
+
+  filteredCustomers: any[] = [];
   customersOrderTotal: number = 0;
-  currencyCode: string = 'USD';
+  currencyCode: string = 'NOK ';
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.filteredCustomers = [
-      {
-        id: 1,
-        name: 'john Doe',
-        city: 'Phoenix',
-        orderTotal: 9.99,
-        customerSince: new Date(2014, 7, 10),
-      },
-      {
-        id: 2,
-        name: 'Jane Doe',
-        city: 'Chandler',
-        orderTotal: 19.99,
-        customerSince: new Date(2017, 2, 22),
-      },
-      {
-        id: 3,
-        name: 'Michelle Thomas',
-        city: 'Seattle',
-        orderTotal: 99.99,
-        customerSince: new Date(2002, 10, 31),
-      },
-      {
-        id: 4,
-        name: 'Jim Thomas',
-        city: 'New York',
-        orderTotal: 599.99,
-        customerSince: new Date(2002, 10, 31),
-      },
-    ];
-  }
+  ngOnInit(): void {}
 
   calculateOrders() {
     this.customersOrderTotal = 0;
     this.filteredCustomers.forEach((customer: ICustomer) => {
       this.customersOrderTotal += customer.orderTotal ?? 0;
     });
+
+    // this.customersOrderTotal = +this.customersOrderTotal.toFixed(2);
+  }
+
+  filter(data: string) {
+    if (data) {
+      this.filteredCustomers = this.customers.filter((customer: ICustomer) => {
+        return (
+          customer.name.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+          customer.city.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+          (customer.orderTotal?.toString().indexOf(data) ?? -1) > -1
+        );
+      });
+    } else {
+      this.filteredCustomers = this.customers;
+    }
+    this.calculateOrders();
+  }
+
+  sort(property: string) {
+    // A sorter service will handle the sorting
   }
 }
