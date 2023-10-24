7210b89f82c4246749a9be48cff363365033e653
Madsemilf <mads@fellkjaer.no>
2023-10-24 15:40:48 +0200

    Imported shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './customers/shared/shared.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppMaterialModule } from './app-material.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './customers/shared/shared.module';
 
 @NgModule({
   imports: [BrowserModule, AppMaterialModule, CustomersModule, SharedModule],
