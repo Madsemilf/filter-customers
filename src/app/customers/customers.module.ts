91079a0c109fa9f1d180b9dd78f934af457bb83a
Madsemilf <mads@fellkjaer.no>
2023-10-24 15:45:27 +0200

    Imported forms and shared module

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from './shared/shared.module';
 import { AppMaterialModule } from '../app-material.module';
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
-  imports: [CommonModule, AppMaterialModule],
+  imports: [CommonModule, AppMaterialModule, SharedModule, FormsModule],
   declarations: [
     CustomersComponent,
     CustomersListComponent,
