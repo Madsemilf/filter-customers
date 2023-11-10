02c71dbf7bb1e1b52fadc4a5cca92bc978802417
Madsemilf <mads@fellkjaer.no>
2023-11-10 10:37:06 +0100

    Added data service to application

---
+++ src/app/core/data.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map, catchError, throwError, Observable, of } from 'rxjs';
+
+import { ICustomer, IOrder } from '../customers/shared/interfaces';
+
+@Injectable()
+export class DataService {
+  baseUrl: string = 'assets/';
+
+  constructor(private http: HttpClient) {}
+
+  getCustomers(): Observable<ICustomer[]> {
+    return this.http
+      .get<ICustomer[]>(this.baseUrl + 'customers.json')
+      .pipe(catchError(this.handleError));
+  }
+
+  // getCustomer(id: number): Observable<ICustomer> {
+  //   return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json').pipe(
+  //     map((customers) => {
+  //       let customer = customers.find((cust: ICustomer) => cust.id === id);
+  //       return customer || null;
+  //     }),
+  //     catchError(this.handleError),
+  //     switchMap((customer) => {
+  //       if (customer) {
+  //         return of(customer);
+  //       } else {
+  //         return of(null); // Return an empty observable if no customer is found
+  //       }
+  //     })
+  //   );
+  // }
+
+  getOrders(id: number): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(this.baseUrl + 'orders.json').pipe(
+      map((orders) => {
+        let custOrders = orders.filter(
+          (order: IOrder) => order.customerId === id
+        );
+        return custOrders;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: any) {
+    console.error('server error:', error);
+    if (error.error instanceof Error) {
+      const errMessage = error.error.message;
+      return throwError(errMessage);
+      // Use the following instead if using lite-server
+      // return throwError(err.text() || 'backend server error');
+    }
+    return throwError(error || 'Node.js server error');
+  }
+}
